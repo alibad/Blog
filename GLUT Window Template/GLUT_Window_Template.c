4caf3e49f667e69f9582d92b451378f0aaad56ac
Ali BaderEddin <ali_badereddin@hotmail.com>
2015-10-08 15:34:21 -0700

    Organizing different GL files

---
+++ GLUT Window Template/GLUT_Window_Template.c
@@ -0,0 +1,352 @@
+//*************************************************************************
+//
+//  File Name	: GLUT Window Template
+//  Author		: Ali BaderEddin
+//  Date		: December 2003
+//  
+//  Description	: Openning an OpenGL window using GLUT library...
+//  
+//*************************************************************************
+
+//  Include GLUT, OpenGL, and GLU libraries
+#include <GL\glut.h>
+
+//  Standard Input\Output C Library
+#include <stdio.h>
+
+//  Initialization
+void init ();
+
+//  Callback functions
+void display (void);
+void reshape (int w, int h);
+void mouse (int button, int state, int x, int y);
+void motion (int x, int y);
+void pmotion (int x, int y);
+void keyboard (unsigned char key, int x, int y);
+void special (int key, int x, int y);
+
+//  Support Functions
+void centerOnScreen ();
+void drawObject ();
+
+//  define the window position on screen
+int window_x;
+int window_y;
+
+//  variables representing the window size
+int window_width = 240;
+int window_height = 240;
+
+//  variable representing the window title
+char *window_title = "GLUT Window Template";
+
+//  Tells whether to display the window full screen or not
+//  Press Alt + Esc to exit a full screen.
+int full_screen = 0;
+
+//-------------------------------------------------------------------------
+//  Set OpenGL program initial state.
+//-------------------------------------------------------------------------
+void init ()
+{	
+	//  Set the frame buffer clear color to black. 
+	glClearColor (0.0, 0.0, 0.0, 0.0);
+}
+
+//-------------------------------------------------------------------------
+//  This function is passed to glutDisplayFunc in order to display 
+//	OpenGL contents on the window.
+//-------------------------------------------------------------------------
+void display (void)
+{
+	//  Clear the window or more specifically the frame buffer...
+	//  This happens by replacing all the contents of the frame
+	//  buffer by the clear color (black in our case)
+	glClear (GL_COLOR_BUFFER_BIT);
+	
+	//  Draw object
+	drawObject ();
+
+	//  Swap contents of backward and forward frame buffers
+	glutSwapBuffers ();
+}
+
+//-------------------------------------------------------------------------
+//  Draws our object.
+//-------------------------------------------------------------------------
+void drawObject ()
+{
+	//  Show when are displaying an object
+	printf ("Displaying object...\n");
+	
+	//  Draw Icosahedron
+	glutWireIcosahedron ();
+}
+
+//-------------------------------------------------------------------------
+//  This function is passed to the glutReshapeFunc and is called 
+//  whenever the window is resized.
+//-------------------------------------------------------------------------
+void reshape (int w, int h)
+{
+	//  Stay updated with the window width and height
+	window_width = w;
+	window_height = h;
+
+	//  Reset viewport
+	glViewport(0, 0, window_width, window_height);
+
+	//  Print current width and height on the screen
+	printf ("Window Width: %d, Window Height: %d.\n", window_width, window_height);
+}
+
+//-------------------------------------------------------------------------
+//  This function is passed to the glutMouseFunc and is called 
+//  whenever the mouse is clicked.
+//-------------------------------------------------------------------------
+void mouse (int button, int state, int x, int y)
+{
+	switch (button)
+	{
+		//  Left Button Clicked
+		case GLUT_LEFT_BUTTON:
+
+			switch (state)
+			{
+				//  Pressed 
+				case GLUT_DOWN:
+					printf ("Mouse Left Button Pressed (Down)...\n");
+					break;
+				//  Released
+				case GLUT_UP:
+					printf ("Mouse Left Button Released (Up)...\n");
+					break;
+			}
+
+			break;
+
+		//  Middle Button clicked
+		case GLUT_MIDDLE_BUTTON:
+			
+			switch (state)
+			{
+				//  Pressed
+				case GLUT_DOWN:
+					printf ("Mouse Middle Button Pressed (Down)...\n");
+					break;
+				//  Released
+				case GLUT_UP:
+					printf ("Mouse Middle Button Released (Up)...\n");
+					break;
+			}
+
+			break;
+
+		//  Right Button Clicked
+		case GLUT_RIGHT_BUTTON:
+			
+			switch (state)
+			{
+				//  Pressed
+				case GLUT_DOWN:
+					printf ("Mouse Right Button Pressed (Down)...\n");
+					break;
+				//  Released
+				case GLUT_UP:
+					printf ("Mouse Right Button Released (Up)...\n");
+					break;
+			}
+
+			break;
+
+	}
+}
+
+//-------------------------------------------------------------------------
+//  This function is passed to the glutMotionFunc and is called 
+//  whenever the mouse is dragged.
+//-------------------------------------------------------------------------
+void motion (int x, int y)
+{
+	//  Print the mouse drag position
+	printf ("Mouse Drag Position: %d, %d.\n", x, y);
+}
+
+//-------------------------------------------------------------------------
+//  This function is passed to the glutPassiveMotionFunc and is called 
+//  whenever the mouse is moved.
+//-------------------------------------------------------------------------
+void pmotion (int x, int y)
+{
+	//  Print mouse move positopn
+	printf ("Mouse Move Position: %d, %d.\n", x, y);
+}
+
+//-------------------------------------------------------------------------
+//  This function is passed to the glutKeyboardFunc and is called 
+//  whenever the user hits a key.
+//-------------------------------------------------------------------------
+void keyboard (unsigned char key, int x, int y)
+{
+	//  Print what key the user is hitting
+	printf ("User is hitting the '%c' key.\n", key);
+	printf ("ASCII code is %d.\n", key);
+	
+	switch (key)
+	{
+		//  User hits A key
+		case 'a':
+			
+			break;
+
+		//  User hits Shift + A key
+		case 'A':
+			
+			break;
+
+		//  User hits Enter
+		case '\r':
+			printf ("User is hitting the Return key.\n"); 
+			break;
+
+		//  User hits Space
+		case ' ':
+			printf ("User is hitting the Space key.\n"); 
+			break;
+
+		//  User hits back space
+		case 8:
+			printf ("User is hitting the Back Space key.\n"); 
+			break;
+
+		//  User hits ESC key
+		case 27:
+			exit (1);
+			break;
+	}
+
+	glutPostRedisplay ();
+}
+
+//-------------------------------------------------------------------------
+//  This function is passed to the glutSpecialFunc and is called 
+//  whenever the user hits a special key.
+//-------------------------------------------------------------------------
+void special (int key, int x, int y)
+{
+	switch (key)
+	{
+		case GLUT_KEY_F1 :
+			printf ("F1 function key.\n"); 
+			break;
+		case GLUT_KEY_F2 :
+			printf ("F2 function key. \n");  
+			break;
+		case GLUT_KEY_F3 :
+			printf ("F3 function key. \n");  
+			break;
+		case GLUT_KEY_F4 :
+			printf ("F4 function key. \n");  
+			break;
+		case GLUT_KEY_F5 :
+			printf ("F5 function key. \n");  
+			break;
+		case GLUT_KEY_F6 :
+			printf ("F6 function key. \n");  
+			break;
+		case GLUT_KEY_F7 :
+			printf ("F7 function key. \n");  
+			break;
+		case GLUT_KEY_F8 :
+			printf ("F8 function key. \n");  
+			break;
+		case GLUT_KEY_F9 :
+			printf ("F9 function key. \n");  
+			break;
+		case GLUT_KEY_F10 :
+			printf ("F10 function key. \n");  
+			break;
+		case GLUT_KEY_F11 :
+			printf ("F11 function key. \n");  
+			break;
+		case GLUT_KEY_F12 :
+			printf ("F12 function key. \n");  
+			break;
+		case GLUT_KEY_LEFT :
+			printf ("Left directional key. \n");  
+			break;
+		case GLUT_KEY_UP :
+			printf ("Up directional key. \n");  
+			break;
+		case GLUT_KEY_RIGHT :
+			printf ("Right directional key. \n");  
+			break;
+		case GLUT_KEY_DOWN :
+			printf ("Down directional key. \n");  
+			break;
+		case GLUT_KEY_PAGE_UP :
+			printf ("Page up directional key. \n");  
+			break;
+		case GLUT_KEY_PAGE_DOWN :
+			printf ("Page down directional key. \n");  
+			break;
+		case GLUT_KEY_HOME :
+			printf ("Home directional key. \n");  
+			break;
+		case GLUT_KEY_END :
+			printf ("End directional key. \n");  
+			break;
+		case GLUT_KEY_INSERT :
+			printf ("Inset directional key. \n");  
+			break;
+	}
+	
+	glutPostRedisplay ();
+}
+
+//-------------------------------------------------------------------------
+//  This function sets the window x and y coordinates
+//  such that the window becomes centered
+//-------------------------------------------------------------------------
+void centerOnScreen ()
+{
+	window_x = (glutGet (GLUT_SCREEN_WIDTH) - window_width)/2;
+	window_y = (glutGet (GLUT_SCREEN_HEIGHT) - window_height)/2;
+}
+
+//-------------------------------------------------------------------------
+//  Program Main method.
+//-------------------------------------------------------------------------
+void main (int argc, char **argv)
+{
+	//  Connect to the windowing system + create a window
+	//  with the specified dimensions and position
+	//  + set the display mode + specify the window title.
+	glutInit(&argc, argv);
+	centerOnScreen();
+	glutInitWindowSize (window_width, window_height);
+	glutInitWindowPosition (window_x, window_y);
+	glutInitDisplayMode (GLUT_RGBA | GLUT_DOUBLE);
+	glutCreateWindow (window_title);
+
+	//  View in full screen if the full_screen flag is on
+	if (full_screen)
+		glutFullScreen ();
+
+	//  Set OpenGL program initial state.
+	init();
+	
+	// Set the callback functions
+	glutDisplayFunc (display);
+	glutReshapeFunc  (reshape);
+	glutMouseFunc (mouse);
+	glutMotionFunc (motion);
+	glutPassiveMotionFunc (pmotion);
+	glutKeyboardFunc (keyboard);
+	glutSpecialFunc (special);
+
+	//  Start GLUT event processing loop
+	glutMainLoop();
+}
+
