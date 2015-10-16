483bb58e7a4c739e48d399e1521a01e2fe97218f
Ali BaderEddin <ali_badereddin@hotmail.com>
2015-10-16 14:59:38 -0700

    C# Regex Helper

---
+++ Windows Forms/Regex Utility/RegexUI.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace RegexUI
+{
+    partial class RegexUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.detailsTextBox = new System.Windows.Forms.RichTextBox();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.matchesList = new System.Windows.Forms.ListView();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.inputTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.regexTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.detailsTextBox);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(385, 160);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Match Details";
+            // 
+            // detailsTextBox
+            // 
+            this.detailsTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.detailsTextBox.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.detailsTextBox.Location = new System.Drawing.Point(3, 16);
+            this.detailsTextBox.Name = "detailsTextBox";
+            this.detailsTextBox.Size = new System.Drawing.Size(379, 141);
+            this.detailsTextBox.TabIndex = 6;
+            this.detailsTextBox.Text = "";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Value";
+            this.columnHeader1.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Index";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Length";
+            // 
+            // matchesList
+            // 
+            this.matchesList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.matchesList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.matchesList.Location = new System.Drawing.Point(3, 16);
+            this.matchesList.MultiSelect = false;
+            this.matchesList.Name = "matchesList";
+            this.matchesList.Size = new System.Drawing.Size(379, 347);
+            this.matchesList.TabIndex = 0;
+            this.matchesList.UseCompatibleStateImageBehavior = false;
+            this.matchesList.View = System.Windows.Forms.View.Details;
+            this.matchesList.SelectedIndexChanged += new System.EventHandler(this.matchesList_SelectedIndexChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.matchesList);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(0, 0);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(385, 366);
+            this.groupBox4.TabIndex = 21;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Matches";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer1.Size = new System.Drawing.Size(752, 530);
+            this.splitContainer1.SplitterDistance = 363;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.groupBox2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer2.Size = new System.Drawing.Size(363, 530);
+            this.splitContainer2.SplitterDistance = 63;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.groupBox4);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.groupBox3);
+            this.splitContainer3.Size = new System.Drawing.Size(385, 530);
+            this.splitContainer3.SplitterDistance = 366;
+            this.splitContainer3.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.inputTextBox);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(363, 463);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Input";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputTextBox.Location = new System.Drawing.Point(3, 16);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(357, 444);
+            this.inputTextBox.TabIndex = 5;
+            this.inputTextBox.Text = "";
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.regexTextBox);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(363, 63);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Regular Expression";
+            // 
+            // regexTextBox
+            // 
+            this.regexTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.regexTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.regexTextBox.Location = new System.Drawing.Point(3, 16);
+            this.regexTextBox.Name = "regexTextBox";
+            this.regexTextBox.Size = new System.Drawing.Size(357, 44);
+            this.regexTextBox.TabIndex = 6;
+            this.regexTextBox.Text = "";
+            this.regexTextBox.TextChanged += new System.EventHandler(this.regexTextBox_TextChanged);
+            // 
+            // RegexUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 530);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "RegexUI";
+            this.Text = "Regular Expression Helper";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            this.splitContainer3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox detailsTextBox;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ListView matchesList;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox inputTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox regexTextBox;
+
+    }
+}
+
---
+++ Windows Forms/Regex Utility/RegexUI.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace RegexUI
+{
+    public partial class RegexUI : Form
+    {
+        public RegexUI()
+        {
+            InitializeComponent();
+        }
+
+        private void regexTextBox_TextChanged(object sender, EventArgs e)
+        {
+            RunRegexCheck();
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            RunRegexCheck();
+        }
+
+        private void RunRegexCheck()
+        {
+            matchesList.BeginUpdate();
+            matchesList.Items.Clear();
+
+            if (regexTextBox.Text != string.Empty)
+            {
+                
+                try
+                {
+                    foreach (Match match in Regex.Matches(inputTextBox.Text, regexTextBox.Text, RegexOptions.Singleline))
+                    {
+                        ListViewItem item = matchesList.Items.Add(match.Value);
+                        item.SubItems.Add(match.Index.ToString());
+                        item.SubItems.Add(match.Length.ToString());
+                        
+                    }
+                }
+                catch (Exception e)
+                {
+                    ListViewItem item = matchesList.Items.Add(e.Message);
+                    item.ForeColor = Color.Red;
+                }
+            }
+
+            matchesList.EndUpdate();
+        }
+
+        private void matchesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (matchesList.SelectedItems.Count > 0)
+            {
+                ListViewItem item = matchesList.SelectedItems[0];
+                detailsTextBox.Text = item.Text;
+            }
+            else
+            {
+                detailsTextBox.Text = string.Empty;
+            }
+        }
+    }
+}
